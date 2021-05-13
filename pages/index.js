7258d08b3aa1682e17841a126a5efd9d2c0fd06a
Al Mondragón <al.drago@live.com>
2021-05-12 22:37:02 -0500

    Add content

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,40 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>Social Proof Section</title>
+        <link rel='icon' href='/favicon.png' />
       </Head>
 
       <main>
-        <p className='text-[red]'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minus
-          debitis vel laborum sequi reiciendis nisi expedita animi temporibus?
-          Cumque vitae temporibus beatae? Minus illo consequatur blanditiis,
-          culpa excepturi officia.
+        <p>10,000+ of our users love our products.</p>
+
+        <p>
+          We only provide great products combined with excellent customer
+          service. See what our satisfied customers are saying about our
+          services.
+        </p>
+
+        <p>
+          Rated 5 Stars in Reviews Rated 5 Stars in Report Guru Rated 5 Stars in
+          BestTech
+        </p>
+
+        <p>
+          Colton Smith Verified Buyer "We needed the same printed design as the
+          one we had ordered a week prior. Not only did they find the original
+          order, but we also received it in time. Excellent!"
+        </p>
+
+        <p>
+          Irene Roberts Verified Buyer "Customer service is always excellent and
+          very quick turn around. Completely delighted with the simplicity of
+          the purchase and the speed of delivery."
+        </p>
+
+        <p>
+          Anne Wallace Verified Buyer "Put an order with this company and can
+          only praise them for the very high standard. Will definitely use them
+          again and recommend them to everyone!"
         </p>
       </main>
     </div>
