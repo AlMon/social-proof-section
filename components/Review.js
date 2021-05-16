59f854373b2e994958a79f805a18acefe9526bfa
Al Mondragón <al.drago@live.com>
2021-05-16 15:30:03 -0500

    Minos review style fixes

--- components/Review.js
+++ components/Review.js
@@ -5,10 +5,15 @@ function Review(props) {
         {Array(props.stars)
           .fill()
           .map((v, i) => (
-            <img src='/icon-star.svg' alt='star' className='inline mx-1' />
+            <img
+              src='/icon-star.svg'
+              alt='star'
+              className='inline mx-1'
+              key={i}
+            />
           ))}
       </span>
-      <span className='text-[#502050] text-[13px] mt-[7px] ml-5'>
+      <span className='text-[#502050] text-[13px] mt-[7px] ml-6'>
         Rated {props.stars} Stars in {props.publication}
       </span>
     </div>
